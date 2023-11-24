a6e6a70125f4960d5226cd48926ab331e21da6a5
tarilabs <matteo.mortari@gmail.com>
2023-11-24 09:13:47 +0100

    test store

---
+++ storesomething.py
@@ -0,0 +1,34 @@
+import kfp
+from kfp_tekton.compiler import TektonCompiler
+
+def os_environ_print(my_input, input2, model_file: kfp.components.OutputPath(),):
+    import os
+    import pickle
+    import json
+    model = dict()
+    model['my_input'] = my_input
+    model['input2'] = input2
+    model['osenviron'] = json.dumps(dict(os.environ))
+    print(model)
+    print(model_file)
+
+    def save_pickle(object_file, target_object):
+        with open(object_file, "w") as f:
+            json.dump(target_object, f)
+            
+    save_pickle(model_file, model)
+
+create_step_os_environ_print = kfp.components.create_component_from_func(
+    func=os_environ_print,
+    base_image='ubi8/python-39',
+    packages_to_install=[])
+
+
+def my_pipeline(my_input):
+  task1 = create_step_os_environ_print(my_input=my_input, input2=kfp.dsl.RUN_ID_PLACEHOLDER)
+
+
+if __name__ == "__main__":
+    TektonCompiler().compile(
+        my_pipeline, __file__.replace(".py", ".yaml")
+    )
